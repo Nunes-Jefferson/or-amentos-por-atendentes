e834770307a58da1d7f014f8da891de6102188b1
Nunes-Jefferson <jefferson.nds7@gmail.com>
2024-08-25 21:28:39 -0300

    Update Proporcao_formafarm_atendente.py

--- Proporcao_formafarm_atendente.py
+++ Proporcao_formafarm_atendente.py
@@ -2,10 +2,17 @@ import firebirdsql
 import pandas as pd
 import streamlit as st
 
+
 # Dados de conexão com o banco
-host = '192.168.1.151'
+
+host = 'localhost'
+host = '172.24.64.1'
 db_fc = 'C:\\Users\\jeffe\\Documents\\Bando de dados Biodiversi\\ALTERDB240620242048.ib'
-user = 'SYSDBA'
+
+host = '192.168.10.102'
+db_fc = 'C:\\FCERTA\\DB\\ALTERDB.IB'
+
+user = 'SYSDBA' 
 password = 'masterkey'
 port = 3050  # porta padrão do Firebird
 
@@ -14,29 +21,28 @@ conexao = firebirdsql.connect(host=host, database=db_fc, user=user, password=pas
 cursor = conexao.cursor()
 
 # Lista de funcionários
-cursor.execute("SELECT CDFUN, NOMEFUN FROM FC08000")
+funcionarios = cursor.execute(f"SELECT CDFUN, NOMEFUN FROM FC08000")
 funcionarios = cursor.fetchall()
 
 st.set_page_config(layout="wide")
 
-# Função para calcular a porcentagem de forma farmacêutica por funcionário
+# calcula a porcetagem de forma farmaceutica por funcionario
 def porc_formaf_por_func(data1, data2, codfunc):
     # TPFORMAFARMA
-    cursor.execute(f"SELECT TPFORMAFARMA FROM FC15100 WHERE DTENTR BETWEEN '{data1}' AND '{data2}' AND CDFUNRE = {codfunc}")
-    orcamentos = cursor.fetchall()
+    req = cursor.execute(f"SELECT TPFORMAFARMA FROM FC15100 WHERE DTENTR BETWEEN '{data1}' AND '{data2}' AND CDFUNRE = {codfunc}")
+    orçamentos = cursor.fetchall()
+
 
     lista_forma_farm = []
-    for item in orcamentos:
+    for item in orçamentos:
         for x in item:
             cursor.execute(f"SELECT FORMA_FARMACEUTICA FROM FC12004 WHERE CODIGO = {x}")
-            forma = cursor.fetchone()[0]
-            lista_forma_farm.append({'codforma': x, 'nomeforma': forma})
+            forma = cursor.fetchall()[0][0]
+            forma_farm = {'codforma': x, 'nomeforma': forma}
+            lista_forma_farm.append(forma_farm)
 
     # Criando o DataFrame
     df = pd.DataFrame(lista_forma_farm)
-
-    if df.empty:
-        return pd.DataFrame(columns=['Forma Farmacêutica', 'Quantidade', 'Porcentagem'])
 
     # Contando as ocorrências de cada forma farmacêutica
     contagem_de_formas = df['nomeforma'].value_counts().reset_index()
@@ -49,22 +55,25 @@ def porc_formaf_por_func(data1, data2, codfunc):
     return contagem_de_formas
 
 # Exibindo a tabela no Streamlit
+
 st.sidebar.header('Orçamentos por funcionários')
 
-data1 = st.sidebar.date_input('Data inicial')
+data1 = st.sidebar.date_input('Data inical')
 data2 = st.sidebar.date_input('Data final')
-lista_func = st.sidebar.multiselect('Selecione o funcionário', funcionarios, format_func=lambda x: x[1])
+lista_func= st.sidebar.multiselect('Selecione o funcionário', funcionarios)
+
 
 col1, col2 = st.columns(2)
-
 for i, (codfunc, funcionario_selecionado) in enumerate(lista_func):
-    contagem_de_formas = porc_formaf_por_func(data1, data2, codfunc)
-    
     if i % 2 == 0:
-        col1.subheader(funcionario_selecionado)
-        col1.text(f'Total de orçamentos: {contagem_de_formas["Quantidade"].sum()}')
+        contagem_de_formas=porc_formaf_por_func(data1, data2, codfunc)
+        col1.text(funcionario_selecionado)
+        col1.text(f'Total de orçamentos: {contagem_de_formas['Quantidade'].sum()}')
         col1.dataframe(data=contagem_de_formas, use_container_width=True)
+    
     else:
-        col2.subheader(funcionario_selecionado)
-        col2.text(f'Total de orçamentos: {contagem_de_formas["Quantidade"].sum()}')
+        contagem_de_formas=porc_formaf_por_func(data1, data2, codfunc)
+        col2.text(funcionario_selecionado)
+        col2.text(f'Total de orçamentos: {contagem_de_formas['Quantidade'].sum()}')
         col2.dataframe(data=contagem_de_formas, use_container_width=True)
+
